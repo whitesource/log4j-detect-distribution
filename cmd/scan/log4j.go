53b8bc8c43caa15c2c9782256538f7cc4b438e8d
nabeel <48175656+nabeelsaabna@users.noreply.github.com>
2021-12-29 15:39:42 +0200

    add CVE-2021-44832 (#5)

--- cmd/scan/log4j.go
+++ cmd/scan/log4j.go
@@ -15,10 +15,10 @@ type Vulnerability struct {
 
 var fixes = map[string]map[string]string{
 	"org.apache.logging.log4j": {
-		"log4j-core": "Upgrade to version org.apache.logging.log4j:log4j-core:2.17.0",
+		"log4j-core": "Upgrade to version org.apache.logging.log4j:log4j-core:2.17.1",
 	},
 	"org.ops4j.pax.logging": {
-		"pax-logging-log4j2": "Upgrade to version org.ops4j.pax.logging:pax-logging-log4j2:2.0.12",
+		"pax-logging-log4j2": "Upgrade to version org.ops4j.pax.logging:pax-logging-log4j2:2.0.13",
 	},
 }
 
